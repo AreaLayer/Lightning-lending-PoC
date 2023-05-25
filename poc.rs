aba1498a63f7012067a677cff52b40031b2a68d2
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-25 12:19:06 -0300

    Update and rename alice.rs to poc.rs

---
+++ poc.rs
@@ -0,0 +1,28 @@
+use ldk::keys::{PrivateKey, PublicKey};
+use nostr_rust::ChannelParameters;
+use rust_dlc::Wallet;
+
+fn main() {
+    // Generate private and public keys for Alice and Bob
+    let alice_private_key = PrivateKey::generate();
+    let bob_private_key = PrivateKey::generate();
+
+    let alice_public_key = PublicKey::from_private_key(&alice_private_key);
+    let bob_public_key = PublicKey::from_private_key(&bob_private_key);
+
+    // Create an open channel between Alice and Bob
+    let channel_parameters = ChannelParameters::new(/* specify channel parameters */);
+    let mut wallet = Wallet::new(channel_parameters.clone());
+    let (funding_tx, channel_id) = wallet.open_channel(&alice_private_key, &bob_public_key);
+
+    // Perform Lightning Network transactions on the open channel
+
+    // Close the channel
+    let closing_tx = wallet.close_channel(&channel_id, &alice_private_key, &bob_private_key);
+
+    // Broadcast the closing transaction to the blockchain
+    // ...
+
+    // Further processing
+    // ...
+}
